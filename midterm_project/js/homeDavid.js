38d9b74da5809402068c733a945f44723ea48940
Tanner Skelton <tskelton@huskers.unl.edu>
2022-03-08 13:00:35 -0600

    Added davids pitures to homepage and made necessary changes to make it work. Created a homeDavid.js and homeTanner.js to house those event listeners.

--- midterm_project/js/homeDavid.js
+++ midterm_project/js/homeDavid.js
@@ -1,12 +1,12 @@
-function mouseOverTanner(){
-    var test = document.getElementById("tanner_img");
+function mouseOverDavid(){
+    var test = document.getElementById("david_img");
     
     test.addEventListener("mousedown", function( event ) {
         // highlight the mouseenter target
-        test.src = "../pictures/tanner_front.PNG";
+        test.src = "../pictures/david_front.PNG";
         // reset the color after a short delay
         setTimeout(function() {
-            event.target.src = "../pictures/tanner_side.PNG";
+            event.target.src = "../pictures/david_side.PNG";
         }, 1000);
     });
 
